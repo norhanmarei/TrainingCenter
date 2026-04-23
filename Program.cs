49adf8748ced06f403745d61dc380ac2c45d12a4
norhanmarei <mustafanour751@gmail.com>
2026-04-23 09:17:08 +0300

    Extract connection string with configuration and pass it to DbContext to test connection

--- Program.cs
+++ Program.cs
@@ -1,1 +1,38 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TrainingCenter.Data;
+
+// Build configuration
+IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+// Read connection string
+string? connectionString = configuration.GetConnectionString("DefaultConnection");
+
+// Validate connection string.
+if (string.IsNullOrWhiteSpace(connectionString))
+{
+    Console.WriteLine("Connection string not found.");
+    return;
+}
+
+
+// Configure EF Core
+var options = new DbContextOptionsBuilder<AppDbContext>()
+    .UseNpgsql(connectionString)
+    .Options;
+
+// Create context
+using var context = new AppDbContext(options);
+
+// Test
+Console.WriteLine("================================================");
+
+Console.WriteLine(context.Database.CanConnect()
+    ? "Connected! You are ready to retrieve Data :-)"
+    : "Failed to connect.");
+
+Console.WriteLine("================================================");
+
